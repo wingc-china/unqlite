93aba0ad3dc53f210b9d7f41fea424b23823aca0
WiNGC <wingc@wingc.net>
2015-08-16 17:03:42 -0700

    Added an unit test project following Visual C++ Unit Test Framework but it is not able to build yet.

---
+++ Projects/Windows/unittest.cpp
@@ -0,0 +1,21 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+#include <UnQLite.h>
+#include <unique_handle.hxx>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest
+{		
+	TEST_CLASS(UnitTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod)
+		{
+			// TODO: Your test code here
+		}
+
+	};
+}
